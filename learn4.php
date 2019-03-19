93ffe6411cb4a24dafce9e4f639496b39b672087
Andy Miller <rhuk@mac.com>
2019-03-19 16:03:48 -0600

    initial commit

---
+++ learn4.php
@@ -0,0 +1,64 @@
+<?php
+namespace Grav\Theme;
+
+use Grav\Common\Grav;
+use Grav\Common\Theme;
+
+class Learn4 extends Theme
+{
+    public static function getSubscribedEvents() {
+        return [
+            'onTwigInitialized' => ['onTwigInitialized', 0],
+            'onTwigPageVariables' => ['onTwigPageVariables', 0],
+            'onTNTSearchQuery' => ['onTNTSearchQuery', 1000],
+        ];
+    }
+
+    public function onTwigPageVariables()
+    {
+        $this->grav['twig']->twig_vars['grav_version'] = GRAV_VERSION;
+    }
+
+    public function onTNTSearchQuery(Event $e)
+    {
+        $query = $this->grav['uri']->param('q');
+
+        if ($query) {
+            $page = $e['page'];
+            $query = $e['query'];
+            $options = $e['options'];
+            $fields = $e['fields'];
+
+            $fields->results[] = $page->route();
+            $e->stopPropagation();
+        }
+    }
+
+    public function onTwigInitialized()
+    {
+        $sc = $this->grav['shortcode'];
+        $sc->getHandlers()->addAlias('version', 'lang');
+
+        $twig = $this->grav['twig'];
+
+        $form_class_variables = [
+//            'form_outer_classes' => 'form-horizontal',
+            'form_button_outer_classes' => 'button-wrapper',
+            'form_button_classes' => 'btn',
+            'form_errors_classes' => '',
+            'form_field_outer_classes' => 'form-group',
+            'form_field_outer_label_classes' => 'form-label-wrapper',
+            'form_field_label_classes' => 'form-label',
+//            'form_field_outer_data_classes' => 'col-9',
+            'form_field_input_classes' => 'form-input',
+            'form_field_textarea_classes' => 'form-input',
+            'form_field_select_classes' => 'form-select',
+            'form_field_radio_classes' => 'form-radio',
+            'form_field_checkbox_classes' => 'form-checkbox',
+        ];
+
+        $twig->twig_vars = array_merge($twig->twig_vars, $form_class_variables);
+
+    }
+
+}
