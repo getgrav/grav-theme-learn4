f57ed0addcb40f0f2f872e275e5e8b537e037201
Andy Miller <rhuk@mac.com>
2019-03-30 14:29:26 -0600

    progress with prism

--- js/learn4.js
+++ js/learn4.js
@@ -1,19 +1,3 @@
-function fallbackMessage(action) {
-    var actionMsg = '';
-    var actionKey = (action === 'cut' ? 'X' : 'C');
-
-    if (/iPhone|iPad/i.test(navigator.userAgent)) {
-        actionMsg = 'No support :(';
-    } else if (/Mac/i.test(navigator.userAgent)) {
-        actionMsg = 'Press ⌘-' + actionKey + ' to ' + action;
-    } else {
-        actionMsg = 'Press Ctrl-' + actionKey + ' to ' + action;
-    }
-
-    return actionMsg;
-}
-
-
 jQuery(document).ready(function () {
 
 
@@ -90,46 +74,6 @@ jQuery(document).ready(function () {
         jQuery('[data-search-input]').val(sessionStorage.getItem('search-value'));
         jQuery('[data-search-input]').trigger('input');
     }
-
-    // clipboard
-    var clipInit = false;
-    $('code').each(function () {
-        var code = $(this),
-            text = code.text();
-
-        if (text.length > 5) {
-            if (!clipInit) {
-                var text, clip = new Clipboard('.copy-to-clipboard', {
-                    text: function (trigger) {
-                        text = $(trigger).prev('code').text();
-                        return text.replace(/^\$\s/gm, '');
-                    }
-                });
-
-                var inPre;
-                clip.on('success', function (e) {
-                    e.clearSelection();
-                    inPre = $(e.trigger).parent().prop('tagName') == 'PRE';
-                    $(e.trigger).attr('aria-label', 'Copied to clipboard!').addClass('tooltipped tooltipped-' + (inPre ? 'w' : 's'));
-                });
-
-                clip.on('error', function (e) {
-                    inPre = $(e.trigger).parent().prop('tagName') == 'PRE';
-                    $(e.trigger).attr('aria-label', fallbackMessage(e.action)).addClass('tooltipped tooltipped-' + (inPre ? 'w' : 's'));
-                    $(document).one('copy', function () {
-                        $(e.trigger).attr('aria-label', 'Copied to clipboard!').addClass('tooltipped tooltipped-' + (inPre ? 'w' : 's'));
-                    });
-                });
-
-                clipInit = true;
-            }
-
-            code.after('<span class="copy-to-clipboard" title="Copy to clipboard" />');
-            code.next('.copy-to-clipboard').on('mouseleave', function () {
-                $(this).attr('aria-label', null).removeClass('tooltipped tooltipped-s tooltipped-w');
-            });
-        }
-    });
 
     // allow keyboard control for prev/next links
     jQuery(function () {
